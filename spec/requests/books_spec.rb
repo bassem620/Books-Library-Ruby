36d5024b15ef7c6538b3ee486776e826931a77aa
Bassem S.Mosaad <bassemsaeed620@gmail.com>
2023-06-12 02:14:37 +0300

    reformating controller and test_spec

--- spec/requests/books_spec.rb
+++ spec/requests/books_spec.rb
@@ -2,33 +2,38 @@ require 'rails_helper'
 
 describe "Books API", type: :request do
     describe "GET /books" do
+        let!(:author) {FactoryBot.create(:author, first_name: "Test author FN", last_name: "test author 1 LN", age: 30)}
         before do
-            FactoryBot.create(:book, title: "Test author 1", author: "Test title 1")
-            FactoryBot.create(:book, title: "Test author 2", author: "Test title 2")
+            FactoryBot.create(:book, title: "Test author 1", author_id: author.id)
+            FactoryBot.create(:book, title: "Test author 2", author_id: author.id)
         end
         it 'Get all books' do
         get '/api/v1/books'
-        puts response.body
         expect(response).to have_http_status(:success)
-        expect(JSON.parse(response.body).size).to eq(6)
+        expect(JSON.parse(response.body).size).to eq(2)
         end
     end
 
     describe "POST /books" do
         it 'create a new book' do
-        expect {
-            post '/api/v1/books', params: { book: { title: "Test author 1", author: "Test title 1"}}
-        }.to change { Book.count }.from(4).to(5)
-        expect(response).to have_http_status(:created)
+            expect {
+                post '/api/v1/books', params: { 
+                    book: { title: "Test author 1"},
+                    author: {first_name: "Andy", last_name: "Weir", age: 48}
+                }
+            }.to change { Book.count }.from(0).to(1)
+            expect(response).to have_http_status(:created)
+            expect(Author.count).to eq(1)
         end
     end
 
     describe "DELETE /book/:id" do
-        let!(:book) {FactoryBot.create(:book, title: "Test author 3", author: "Test title 3")}
+        let!(:author) {FactoryBot.create(:author, first_name: "Test author FN", last_name: "test author 1 LN", age: 30)}
+        let!(:book) {FactoryBot.create(:book, title: "Test author 3", author_id: author.id )}
         it 'delete a book' do
             expect {
                 delete "/api/v1/books/#{book.id}"
-            }.to change { Book.count }.from(5).to(4)
+            }.to change { Book.count }.from(1).to(0)
             expect(response).to have_http_status(:no_content)
         end
     end
