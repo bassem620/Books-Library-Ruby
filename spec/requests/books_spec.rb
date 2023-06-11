3d415796e40fa54f87e4bda7b0c70ec68720ff5a
Bassem S.Mosaad <bassemsaeed620@gmail.com>
2023-06-12 02:45:40 +0300

    refactoring rspec & response_body & request_helper

--- spec/requests/books_spec.rb
+++ spec/requests/books_spec.rb
@@ -1,16 +1,35 @@
 require 'rails_helper'
 
 describe "Books API", type: :request do
+
+    let!(:first_author) {FactoryBot.create(:author, first_name: "Test author 1 FN", last_name: "test author 1 LN", age: 30)}
+    let!(:second_author) {FactoryBot.create(:author, first_name: "Test author 2 FN", last_name: "test author 2 LN", age: 45)}
+
     describe "GET /books" do
-        let!(:author) {FactoryBot.create(:author, first_name: "Test author FN", last_name: "test author 1 LN", age: 30)}
         before do
-            FactoryBot.create(:book, title: "Test author 1", author_id: author.id)
-            FactoryBot.create(:book, title: "Test author 2", author_id: author.id)
+            FactoryBot.create(:book, title: "Test book 1", author_id: first_author.id)
+            FactoryBot.create(:book, title: "Test book 2", author_id: second_author.id)
         end
         it 'Get all books' do
-        get '/api/v1/books'
-        expect(response).to have_http_status(:success)
-        expect(JSON.parse(response.body).size).to eq(2)
+            get '/api/v1/books'
+            expect(response).to have_http_status(:success)
+            expect(response_body.size).to eq(2)
+            expect(response_body).to eq(
+                [
+                    {
+                        'id' => 1,
+                        'title' => 'Test book 1',
+                        'author_name' => 'Test author 1 FN test author 1 LN',
+                        'author_age' => 30
+                    },
+                    {
+                        'id' => 2,
+                        'title' => 'Test book 2',
+                        'author_name' => 'Test author 2 FN test author 2 LN',
+                        'author_age' => 45
+                    }
+                ]
+            )
         end
     end
 
@@ -18,18 +37,25 @@ describe "Books API", type: :request do
         it 'create a new book' do
             expect {
                 post '/api/v1/books', params: { 
-                    book: { title: "Test author 1"},
+                    book: { title: "Test book"},
                     author: {first_name: "Andy", last_name: "Weir", age: 48}
                 }
             }.to change { Book.count }.from(0).to(1)
             expect(response).to have_http_status(:created)
-            expect(Author.count).to eq(1)
+            expect(Author.count).to eq(3)
+            expect(response_body).to eq(
+                {
+                    'id' => 1,
+                    'title' => 'Test book',
+                    'author_name' => 'Andy Weir',
+                    'author_age' => 48
+                }
+            )
         end
     end
 
     describe "DELETE /book/:id" do
-        let!(:author) {FactoryBot.create(:author, first_name: "Test author FN", last_name: "test author 1 LN", age: 30)}
-        let!(:book) {FactoryBot.create(:book, title: "Test author 3", author_id: author.id )}
+        let!(:book) {FactoryBot.create(:book, title: "Test author 3", author_id: first_author.id )}
         it 'delete a book' do
             expect {
                 delete "/api/v1/books/#{book.id}"
