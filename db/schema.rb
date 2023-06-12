abb90aa13cb69a382db538c0f186cf439c47150a
Bassem S.Mosaad <bassemsaeed620@gmail.com>
2023-06-12 15:40:24 +0300

    Authentication and password encryption

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_12_033531) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_12_121856) do
   create_table "authors", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
@@ -31,6 +31,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_12_033531) do
     t.string "username"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
