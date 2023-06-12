e8f956e29984b5bca53047117b47459505a3b30b
Bassem S.Mosaad <bassemsaeed620@gmail.com>
2023-06-12 05:24:45 +0300

    Testing active job

--- app/jobs/update_sku_job.rb
+++ app/jobs/update_sku_job.rb
@@ -1,14 +1,14 @@
+require 'net/http'
+
 class UpdateSkuJob < ApplicationJob
   queue_as :default
 
-  def perform(book_params)
+  def perform(book_name)
     uri = URI('http://localhost:4567/update_sku')
-    req = Net::HTTP::Post.new(uri, 'Content-Type' => 'application/json').
-    req.body = {sku: '123', name: book_params[:title]}.to_json
+    req = Net::HTTP::Post.new(uri, 'Content-Type' => 'application/json')
+    req.body = {sku: '123', name: book_name}.to_json
     res = Net::HTTP.start(uri.hostname, uri.port) do |http|
       http.request(req)
     end
-
-    raise 'exit'
   end
 end
